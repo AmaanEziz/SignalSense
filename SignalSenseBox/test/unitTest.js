66db619a2068913ae23d71c6f05c998e6d3b1f2d
Reagan Hoang <reaganhoang99@gmail.com>
2022-04-03 17:32:56 -0700

    debug

--- SignalSenseBox/test/unitTest.js
+++ SignalSenseBox/test/unitTest.js
@@ -51,84 +51,113 @@ const lightController = LightController();
 //         })
 //     })
 // })
+let nodeID = '1bfd50dc-b312-11ec-a7b9-0242ac120002';
 
-describe('Test GEt /api/node', () => {
-    it('it should GET node', (done) => {
+describe('Test GEt /api/node/list', () => {
+    it('it should GET Nodes List', (done) => {
       chai.request('http://localhost:3000')  
     //   chai.request(server)
-          .get('/api/node')
+          .get('/api/node/list')
           .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                // res.body.length.should.be.eql(0);
-                res.body.should.have.property('nodeID');
-                res.body.should.have.property('nodeDescription');
-                res.body.should.have.property('intersectionID');
-                res.body.should.have.property('ipAddress');
-                res.body.should.have.property('isAlive');
+                console.log(res.body);
+                res.body.should.be.a('array');
+                res.body[0].should.have.property('nodeID');
+                // nodeID = res.body[0].nodeID;
+                res.body[0].should.have.property('nodeDescription');
+                res.body[0].should.have.property('intersectionID');
+                res.body[0].should.have.property('ipAddress');
+                res.body[0].should.have.property('isAlive');
                 // res.body.errors.pages.should.have.property('kind').eql('required');
             done();
           });
     });
 });
 
-describe('Test GEt /api/node/light', () => {
+console.log(nodeID)
+describe(`Test GEt /api/node/light?nodeId=${nodeID}`, () => {
     it('it should GET all light', (done) => {
       chai.request('http://localhost:3000')  
     //   chai.request(server)
-          .get('/api/node/light')
+          .get(`/api/node/light?nodeId=${nodeID}`)
           .end((err, res) => {
                 res.should.have.status(200);
-                res.body.should.be.a('object');
-                // res.body.length.should.be.eql(0);
-                res.body.should.have.property('lightID');
-                res.body.should.have.property('nodeID');
-                res.body.should.have.property('lightPhase');
-                res.body.should.have.property('lightRowID');
-                res.body.should.have.property('state');
+                console.log(res.body);
+                if (err) return done(err);
+                res.body.should.be.a('array');
+                res.body[0].should.have.property('lightID');
+                res.body[0].should.have.property('nodeID');
+                res.body[0].should.have.property('lightPhase');
+                res.body[0].should.have.property('lightRowID');
+                res.body[0].should.have.property('state');
                 // res.body.errors.pages.should.have.property('kind').eql('required');
+                done();
+          });
+    });
+});
+
+describe(`Test PATCH /api/node/setLights?node_id=${nodeID}`, () => {
+    // it('it should not patchLights a light without state field', (done) => {
+    //     let light = {
+    //         lightID: '1',
+    //         lightPhase: 6
+    //     }
+    // //   chai.request(server)
+    //   chai.request('http://localhost:3000')
+    //       .post('/api/node/light')
+    //       .send(light)
+    //       .end((err, res) => {
+    //             res.should.have.status(200);
+    //             res.body.should.be.a('object');
+    //             res.body.should.have.property('errors');
+    //             res.body.errors.should.have.property('state');
+    //         done();
+    //       });
+    // });
+    it('it should PATCH  light', (done) => {
+        let light = [
+                {
+                    lightID: '152',
+                    lightPhase: 6,
+                    state: 'RED'
+                }
+            ]
+    //   chai.request(server)
+      chai.request('http://localhost:3000')
+          .patch(`/api/node/setLights?node_id=${nodeID}`)
+          .send(light)
+          .end((err, res) => {
+                // res.should.have.status(200);
+                if (err) return done(err);
+                console.log(res.body);
+                res.body.should.be.a('array');
+                // res.body.should.have.property('lightID').eql('152');
+                res.body.should.have.property('lightID');
+                res.body.should.have.property('lightPhase');
+                res.body.should.have.property('state');
             done();
           });
     });
 });
 
-describe('Test POST /api/node/light', () => {
-    it('it should not POST a light without state field', (done) => {
-        let light = {
-            lightID: '1',
-            lightPhase: 6
-        }
-    //   chai.request(server)
-      chai.request('http://localhost:3000')
-          .post('/api/node/light')
-          .send(light)
-          .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('errors');
-                res.body.errors.should.have.property('state');
-                res.body.errors.pages.should.have.property('kind').eql('required');
-            done();
-          });
-    });
-    it('it should POST a light', (done) => {
-        let light = {
-            lightID: '1',
-            lightPhase: 6,
-            state: 'RED'
-        }
-    //   chai.request(server)
-      chai.request('http://localhost:3000')
-          .post('/api/node/light')
-          .send(light)
-          .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('message').eql('Book successfully added!');
-                res.body.should.have.property('lightID');
-                res.body.should.have.property('lightPhase');
-                res.body.should.have.property('state');
-            done();
-          });
-    });
-});
+// describe('Test GEt /api/node/light', () => {
+//     it('it should GET all light', (done) => {
+//       chai.request('http://localhost:3000')  
+//     //   chai.request(server)
+//           .get('/api/node/light')
+//           .end((err, res) => {
+//                 // res.should.have.status(200);
+//                 if (err) return done(err);
+//                 res.body.should.be.a('object');
+                
+//                 // res.body.length.should.be.eql(0);
+//                 // res.body.should.have.property('lightID');
+//                 // res.body.should.have.property('nodeID');
+//                 // res.body.should.have.property('lightPhase');
+//                 // res.body.should.have.property('lightRowID');
+//                 // res.body.should.have.property('state');
+//                 // res.body.errors.pages.should.have.property('kind').eql('required');
+//                 done();
+//           });
+//     });
+// });
