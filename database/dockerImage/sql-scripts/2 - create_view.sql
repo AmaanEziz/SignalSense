8140cb20d3d9c5736493c978b3e3462b5204c13a
Michael Ingrum <mikeingrum1@gmail.com>
2022-04-21 17:26:43 -0700

    # Bug Fixes - no issue num # updated stream to work on backend, not just nodeControllers

--- database/dockerImage/sql-scripts/2 - create_view.sql
+++ database/dockerImage/sql-scripts/2 - create_view.sql
@@ -1,20 +1,11 @@
 create view Phase_vw as (
-    select 
-        Phase.intersectionID, Phase.phaseRowId , coalesce(lt.state, 'UNDEFINED') state
-    from 
-        Phase
-        left outer join Light lt on Phase.phaseRowId = lt.lightPhase and exists (select * from Node, Light where Node.nodeID = Light.nodeID and Light.lightID = lt.lightID)
-    where 1=1
-     GROUP BY phaseRowId, intersectionID, state
-     order by intersectionID, phaseRowId
+	select 
+		Phase.intersectionID, Phase.phaseRowId, coalesce((if(lt.state = 0, null, lt.state)), 'UNDEFINED') state
+	from  
+		Phase
+		inner join Node on Node.intersectionID = Phase.intersectionID
+		left outer join Light lt on Phase.phaseRowId = lt.lightPhase and lt.nodeID = Node.nodeID -- and exists (select * from Node, Light where Node.nodeID = Light.nodeID and Light.lightID = lt.lightID)
+	where 1=1
+	 GROUP BY phaseRowId, intersectionID, state
+	 order by Phase.intersectionID, phaseRowId
 );
-
--- drop function if exists get_image_state;
--- DELIMITER $$
--- create function get_image_state(p_nodeID varchar(36))
--- returns varchar(100)
--- /*NOT DETERMINISTIC*/
--- begin
--- 	return (select concat(p_nodeID,'_', group_concat(state, lightPhase order by lightRowID desc SEPARATOR '')) from Light where nodeID = p_nodeID);
--- end$$
--- DELIMITER ;
