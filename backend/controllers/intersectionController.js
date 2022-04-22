f2887b25773617d3491010d16b93f7f0ebd70488
Michael Ingrum <mikeingrum1@gmail.com>
2022-04-21 17:25:21 -0700

    # DT-302 # Added ablility to get stream via the rest API. (for demo website) + controller + router

--- backend/controllers/intersectionController.js
+++ backend/controllers/intersectionController.js
@@ -13,7 +13,21 @@ function intersectionController() {
           }
         });
     }
-    return { updateAll };
+    function getStream(req, res){
+      var { intersectionID } = req.body;
+      const query = "CALL get_phase_stream(?)";
+      pool.query(query, [intersectionID], (error, results) => {
+        if (error) {
+          console.log(error);
+          return res.status(501).send('Unable to generate the stream.\nContact Support');
+        }
+        else {
+          console.log(results);
+          return res.status(200).JSON(results);
+        }
+      });
+    }
+    return { updateAll, getStream };
   }
   
   module.exports = intersectionController;
--- backend/routes/intersectionRouter.js
+++ backend/routes/intersectionRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/intersectionController")();
 function routes(temp){
     router.put("/updateAll", controller.updateAll);
+    router.get("/stream", controller.getStream);
     return router
 }
 module.exports = routes;
