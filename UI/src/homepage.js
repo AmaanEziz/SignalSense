175105426ffb1678d5d7411085e04fa17af9a43b
Amaan Eziz <amaaneziz75@gmail.com>
2023-02-11 11:46:12 -0800

    FIX: keep only street intersections that work in the intersection search bar.

--- UI/src/homepage.js
+++ UI/src/homepage.js
@@ -2,9 +2,11 @@ var $dropdown = $("#device-option");
 
 $.getJSON('https://signalsense.link/api/node/list', function(data){
     console.log(data);
-
-    $.each(data, function(){
-        $dropdown.append($('<option />').val(this.nodeID).text(`Device: ${this.nodeDescription}`));
+    let ONLY_WORKING_NODE_ID ='8f65b638-c41b-11ec-a763-0242ac120002'
+    $.each(data, function () {
+        if (this.nodeID = ONLY_WORKING_NODE_ID) {
+            $dropdown.append($('<option />').val(this.nodeID).text(`Device: ${this.nodeDescription}`));
+        }
     });
 });
 
