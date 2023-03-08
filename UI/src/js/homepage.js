d9cd5926b7ae18e02d51af722832d7ccb03a3057
Amaan Eziz <amaaneziz75@gmail.com>
2023-03-07 20:11:31 -0800

    FIX: fix session storage bug Session storage was not retaining the correct nodeID, leading to errors when viewing intersections.

--- UI/src/js/homepage.js
+++ UI/src/js/homepage.js
@@ -3,10 +3,11 @@ var $dropdown = $("#device-option");
 $.getJSON('https://signalsense.link/api/node/list', function(data){
     let ONLY_WORKING_NODE_ID ='8f65b638-c41b-11ec-a763-0242ac120002'
     $.each(data, function () {
-            $dropdown.append($('<option />').val(this.nodeID).text(`Device: ${this.nodeDescription}`));
+        console.log($("#device-option"))
+        $("#device-option").append(`<option value=${this.nodeID}>${this.nodeDescription}</option>`)
     });
 });
 
 function saveSelectedValue() {
-    sessionStorage.setItem('nodeId', $dropdown.val());
+    sessionStorage.setItem('nodeId', $("#device-option").val());
 }
