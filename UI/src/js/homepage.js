5722c7741c1d0c76e590493e39f59591d15bd1c1
Amaan Eziz <amaaneziz75@gmail.com>
2023-03-07 19:58:08 -0800

    FEAT: Re-add intersections list. Readding intersections for Duy to test rather or not turning SignalBox will make the other intersections work.

--- UI/src/js/homepage.js
+++ UI/src/js/homepage.js
@@ -3,9 +3,7 @@ var $dropdown = $("#device-option");
 $.getJSON('https://signalsense.link/api/node/list', function(data){
     let ONLY_WORKING_NODE_ID ='8f65b638-c41b-11ec-a763-0242ac120002'
     $.each(data, function () {
-        if (this.nodeID == ONLY_WORKING_NODE_ID) {//This conditional statement should be removed later when the intersections are active
             $dropdown.append($('<option />').val(this.nodeID).text(`Device: ${this.nodeDescription}`));
-        }
     });
 });
 
