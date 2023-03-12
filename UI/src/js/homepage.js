0d131cad432956647dd0106dfaa472f67b1a64aa
Amaan Eziz <amaaneziz75@gmail.com>
2023-03-12 11:18:22 -0700

    SS8-121: Create the upload features of the website Resolve CORS issue. Accept only photo images when uploading file. Display error message if invalid URL provided. Remove useless input fields.

--- UI/src/js/homepage.js
+++ UI/src/js/homepage.js
@@ -1,9 +1,7 @@
 var $dropdown = $("#device-option");
 
 $.getJSON('https://signalsense.link/api/node/list', function(data){
-    let ONLY_WORKING_NODE_ID ='8f65b638-c41b-11ec-a763-0242ac120002'
     $.each(data, function () {
-        console.log($("#device-option"))
         $("#device-option").append(`<option value=${this.nodeID}>${this.nodeDescription}</option>`)
     });
 });
