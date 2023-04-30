28f4e51ad75f4342ce3d52e433b2d2003ecd78e0
jchen42 <91099706+jchen42@users.noreply.github.com>
2023-04-30 14:50:56 -0700

    SS8-162: Implement background color picker

--- UI/src/js/homepage.js
+++ UI/src/js/homepage.js
@@ -6,3 +6,16 @@ function saveSelectedValue() {
     sessionStorage.setItem('nodeId', $("#device-option").val());
     
 }
+
+<html>
+<body>
+   <h2>Using the <i>input event</i> to change a background color via color picker</h2>
+   <input id = "colorInput" type = "color">
+   <script>
+      let colorInput = document.getElementById('colorInput');
+      colorInput.addEventListener('input', () =>{
+         document.body.style.backgroundColor = colorInput.value;
+      });
+   </script>
+</body>
+</html>
